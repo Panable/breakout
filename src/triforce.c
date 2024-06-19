59154144f6208f365b1f87d62369146f5d9a06ac
Dhanveer Ramnauth <dhanr1606@gmail.com>
2024-06-19 13:35:09 +1000

    added null terminator

--- src/triforce.c
+++ src/triforce.c
@@ -147,6 +147,7 @@ char* file_to_buffer(const char* file_name)
     char* buffer = malloc(length + 1); // allocate space for file length + nullchar
 
     fread(buffer, 1, length, file);    // Read the file into buffer
+    buffer[length] = '\0';
     fclose(file);
     return buffer;
 }
