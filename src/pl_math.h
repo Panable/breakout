7df67441485f7d6dcedd5347b86300f8aa54ce05
Dhanveer Ramnauth <dhanr1606@gmail.com>
2024-06-25 00:41:24 +1000

    multiplication

--- src/pl_math.h
+++ src/pl_math.h
@@ -1,28 +1,52 @@
+/* Column Major Ordering Matrix Library */
 #ifndef PL_MATH_H
 #define PL_MATH_H
-#define PL_CLAMP(x, low, high)  (((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
+
+#include <math.h>
+
+#define PLM_CLAMP(x, low, high)  (((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
+
+#define PI 3.14159265358979323846f
+#define DEG2RAD (PI/180.0f)
+#define RAD2DEG (180.0f/PI)
 
 /* Vectors */
-typedef float vec2f[2];
 typedef float vec3f[3];
-typedef float vec4f[4];
+typedef float vec2f[3];
 
 /* Matrices */
-typedef vec2f mat2f[2];
 typedef vec3f mat3f[3];
-typedef vec4f mat4f[4];
 
-void plm_mat2f_identity(mat2f mat);
+/* Vector Operations */
+void plm_vec2f_scale(vec2f vec, vec2f amt);
+void plm_vec2f_translate(vec2f vec, vec2f amt);
+void plm_vec2f_rotate(vec2f vec, float rad);
+
 void plm_mat3f_identity(mat3f mat);
-void plm_mat4f_identity(mat4f mat);
 
 void plm_mat3f_mul(mat3f a, mat3f b, mat3f dest);
+void plm_mat3f_add(mat3f a, mat3f b, mat3f dest);
 
 void plm_mat3f_dump(mat3f mat);
-/* Column Major Ordering */
+
+
 #ifdef PL_MATH_IMPLEMENTATION
 
-void plm_mat2f_identity(mat2f mat)
+void plm_vec2f_scale(vec2f vec, vec2f amt, vec2f dest)
+{
+    vec3f scratch = {
+        vec[0],
+        vec[1],
+        1,
+    };
+}
+
+void plm_vec2f_translate(vec2f vec, vec2f amt)
+{
+    
+}
+
+void plm_vec2f_rotate(vec2f vec, float rad)
 {
 }
 
@@ -33,26 +57,34 @@ void plm_mat3f_identity(mat3f mat)
     mat[2][0] = 0.0f;    mat[2][1] = 0.0f;    mat[2][2] = 1.0f;
 }
 
-void plm_mat4f_identity(mat4f mat)
-{
-}
-
 void plm_mat3f_mul(mat3f a, mat3f b, mat3f dest)
 {
     float a00 = a[0][0], a01 = a[0][1], a02 = a[0][2],
           a10 = a[1][0], a11 = a[1][1], a12 = a[1][2],
-          a20 = a[2][0], a01 = a[2][1], a22 = a[2][2];
+          a20 = a[2][0], a21 = a[2][1], a22 = a[2][2];
 
     float b00 = b[0][0], b01 = b[0][1], b02 = b[0][2],
           b10 = b[1][0], b11 = b[1][1], b12 = b[1][2],
-          b20 = b[2][0], b01 = b[2][1], b22 = b[2][2];
+          b20 = b[2][0], b21 = b[2][1], b22 = b[2][2];
+
+    dest[0][0] = b00 * a00 + b01 * a10 + b02 * a20;
+    dest[0][1] = b00 * a01 + b01 * a11 + b02 * a21;
+    dest[0][2] = b00 * a02 + b01 * a12 + b02 * a22;
+
+    dest[1][0] = b10 * a00 + b11 * a10 + b12 * a20;
+    dest[1][1] = b10 * a01 + b11 * a11 + b12 * a21;
+    dest[1][2] = b10 * a02 + b11 * a12 + b12 * a22;
+
+    dest[2][0] = b20 * a00 + b21 * a10 + b22 * a20;
+    dest[2][1] = b20 * a01 + b21 * a11 + b22 * a21;
+    dest[2][2] = b20 * a02 + b21 * a12 + b22 * a22;
 }
 
 void plm_mat3f_dump(mat3f mat)
 {
-    printf("%.2f, %.2f, %.2f\n", mat[0][0], mat[0][1], mat[0][2]);    
-    printf("%.2f, %.2f, %.2f\n", mat[1][0], mat[1][1], mat[1][2]);    
-    printf("%.2f, %.2f, %.2f\n", mat[2][0], mat[2][1], mat[2][2]);    
+    printf("\t%.2f, %.2f, %.2f\n", mat[0][0], mat[0][1], mat[0][2]);    
+    printf("\t%.2f, %.2f, %.2f\n", mat[1][0], mat[1][1], mat[1][2]);    
+    printf("\t%.2f, %.2f, %.2f\n", mat[2][0], mat[2][1], mat[2][2]);    
 }
 
 #endif // PL_MATH_IMPLEMENTATION
--- src/transforms.c
+++ src/transforms.c
@@ -8,21 +8,39 @@ int main(void)
     printf("%.2f\n", test);
     mat3f matrix;
 
+    vec3f test_vec = 
+    {
+        1.0f,
+        2.0f,
+        3.0f,
+    };
+
     mat3f a =
     {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9},
+        {1, 4, 7},
+        {2, 5, 8},
+        {3, 6, 9},
     };
 
     mat3f b =
     {
-        {10, 11, 12},
-        {13, 14, 15},
-        {16, 17, 9},
+        {10, 13, 16},
+        {11, 14, 17},
+        {12, 15, 18},
     };
+
+    printf("Matrix A:\n");
+    plm_mat3f_dump(a);
+    printf("Matrix B:\n");
+    plm_mat3f_dump(b);
+
     plm_mat3f_identity(matrix);
     plm_mat3f_mul(a, b, matrix);
     plm_mat3f_dump(matrix);
+
+    float* mat_data = (float*)matrix;
+
+    printf("The first data block is %f", *(mat_data + 1));
+
     return 0;
 }
